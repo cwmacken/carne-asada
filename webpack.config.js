80d96d86a4d5a8857d5d9512dc3f07de33fadd2f
Conor Macken <cwmacken@gmail.com>
2017-07-24 19:54:55 +0800

    update to web pack 2.0

--- webpack.config.js
+++ webpack.config.js
@@ -6,47 +6,44 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    devtool: 'eval-source-map',
-    entry: [
-        'webpack-hot-middleware/client?reload=true',
-        path.join(__dirname, 'src/main.jsx')
-    ],
-    output: {
-        path: path.join(__dirname, '/dist/'),
-        filename: '[name].js',
-        publicPath: '/'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.tpl.html'
-        }),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin(),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('local')
-        }),
-        new ExtractTextPlugin('public/style.css', {
-            allChunks: true
-        })
-    ],
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel',
-            query: {
-                "presets": ["react", "es2015"]
-            }
-        }, {
-            test: /\.(jpg|png)$/,
-            loader: 'file-loader?name=[path][name].[hash].[ext]'
-        }, {
-            test: /\.scss$/,
-            loader: ExtractTextPlugin.extract('css!sass')
-        }, {
-            test: /\.less$/,
-            loader: "style-loader!css-loader!less-loader"
-        }]
-    }
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'src/main.jsx')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.tpl.html'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('local')
+    }),
+    new ExtractTextPlugin({
+      filename: 'public/style.css',
+      allChunks: true
+    })
+  ],
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        "presets": ["react", "es2015"]
+      }
+    }, {
+      test: /\.(jpg|png)$/,
+      loader: 'file-loader?name=[path][name].[hash].[ext]'
+    }, {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+    }]
+  }
 };
